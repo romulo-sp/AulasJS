9c0e6838a2e8dded3894c2e3d423293053d5462b
Romulo Santana <romulo.ribeiro.ps@gmail.com>
2020-04-19 19:43:47 -0300

    inicio e exercicios em NODE.JS

---
+++ Node/pastaA/PastaB/acessaModulo.js
@@ -0,0 +1,13 @@
+const moduloA = require('../../moduloA')
+const c = require('./pastaC')
+
+console.log(moduloA.ola)
+
+const http = require('http')
+
+// http.createServer((require ,response ) => {
+//     response.write('Bom dia!')
+//     response.end()
+// }).listen (8080)
+
+console.log(c.ola2)
